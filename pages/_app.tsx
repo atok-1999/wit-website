ccf9da94d9f0a7e6235fdf73c0abbb027a83e079
Fumiya Sato <fumist1028@gmail.com>
2020-09-26 15:16:04 +0900

    removed unneccessary parts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
-import { AppProps, Container } from 'next/app';
+import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Header from 'src/components/organisms/Header';
-import { ThemeProvider} from '@material-ui/core/styles';
-import { createMuiTheme } from '@material-ui/core/styles';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
-    },
-  },
-});
+import Footer from 'src/components/organisms/Footer';
+import MaterialThemeProvider from 'src/components/MaterialThemeProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ThemeProvider theme={theme}>
+      <MaterialThemeProvider>
         <Header />
         <Component {...pageProps} />
-      </ThemeProvider>
+        <Footer />
+      </MaterialThemeProvider>
   )
 }
 
