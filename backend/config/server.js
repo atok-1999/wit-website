03d872ea718a8db3b003bca7636ec71fdc4f5451
Fumiya Sato <fumist1028@gmail.com>
2020-09-26 17:11:24 +0900

    removed files

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  admin: {
-    auth: {
-      secret: process.env.ADMIN_JWT_SECRET,
-    },
-  },
-});
