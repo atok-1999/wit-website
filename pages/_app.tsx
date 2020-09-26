cdffd38214387834769b7beea980f2430905944e
Fumiya Sato <fumist1028@gmail.com>
2020-09-26 17:01:21 +0900

    add NoSsr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Header from 'src/components/organisms/Header';
-import Footer from 'src/components/organisms/Footer';
 import MaterialThemeProvider from 'src/components/MaterialThemeProvider';
+import NoSsr from '@material-ui/core/NoSsr';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <MaterialThemeProvider>
-        <Header />
+        {/* ここはちゃんと調べる。_app.tsxと_document.tsxについて */}
+        <NoSsr>
+         <Header />
+        </NoSsr>
         <Component {...pageProps} />
-        <Footer />
       </MaterialThemeProvider>
   )
 }
