ac6f8d2dba710ff830f740c63b79bdfa7e5933a2
Kota Ueda <kotainjp@gmail.com>
2020-11-08 18:00:55 +0900

    いらないコードをとりあえず整理

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,11 @@
 import * as React from 'react';
 import Home from 'src/components/organisms/Home';
 
-// import useFetchData from 'src/api/useFetchData';
-
 const Index = () => {
-  // const [data, setData] = useFetchData('news');
 
   return (
     <div>
-      {/* {data.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
-          <small>{item.date}</small>
-          <p>{item.description}</p>
-        </div>
-      ))} */}
-      <Home />
+      <Home></Home>
     </div>
   );
 }
