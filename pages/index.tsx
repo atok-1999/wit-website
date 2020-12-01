db70a3e35205c072e886b953352c8383a7196ccd
Kota Ueda <kotainjp@gmail.com>
2020-12-01 20:12:48 +0900

    ホームページとAboutとActivitiesが9割型完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,19 @@ import Home from 'src/components/organisms/Home';
 import Header from 'src/components/organisms/Header';
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles((theme) => ({
   container: {
-    backgroundColor: theme.palette.primary.main
-  }
-}))
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
 
 const Index = () => {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div className={classes.container}>
-      <Header></Header>  
       <Home></Home>
     </div>
   );
-}
+};
 
 export default Index;
