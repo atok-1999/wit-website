b72399694ba3de78aba6200708bfa2b014681523
Fumiya Sato <fumist1028@gmail.com>
2021-01-08 18:43:07 +0900

    created form

---
+++ pages/api/form.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+module.exports = async (req: NextApiRequest, res: NextApiResponse) => {
+    const values = req.body;
+
+    console.log("values: ", values)
+
+    const text = `
+==========================================================
+会社名： ${values.company}
+名前： ${values.name}
+Email: ${values.mail}
+電話番号: ${values.phoneNumber}
+内容: ${values.content}
+==========================================================
+    `
+
+    const attachment = {
+        author_name: 'お問い合わせbot'
+    }
+
+    const data = {
+        text: text,
+        attachments: JSON.stringify(attachment) || [],
+    }
+
+    if (req.method === 'POST') {
+        try {
+            await axios({
+                method: 'post',
+                url: 'https://hooks.slack.com/services/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',
+                data: data
+            })
+            res.writeHead(201).end('Created');
+            return;
+        }
+        catch(err) {
+            console.error(err);
+            return;
+        }
+    } else {
+        res.writeHead(405).end('Method not allowed')
+    }
+}
